6531a57ba2c35d28bf04e92d0069850a87e238e7
nelmish <standarduser@fitnessunchained.de>
2021-07-22 20:04:30 +0200

    144. Praxis 30: Das Eingabeformular anlegen

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
+eingabeformular.anzeigen();
 
-
-haushaltsbuch.eintrag_hinzufuegen();
-console.log(haushaltsbuch);
+//haushaltsbuch.eintrag_hinzufuegen();
+//console.log(haushaltsbuch);
